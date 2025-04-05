355bcec4d63ff592bb6f20dbf3576d55b91fb740
octavioromagnoli <octaromag@gmail.com>
2025-04-05 04:26:51 -0300

    Format changes in ej1

--- ej1/Time.cpp
+++ ej1/Time.cpp
@@ -1,70 +1,79 @@
 #include "Time.h"
-#include<iostream>
-#include<stdexcept>
-#include<iomanip>
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
 using namespace std;
 
-Time::Time(int hours, int minutes, int seconds, string meridiem): hours(hours), minutes(minutes), seconds(seconds), meridiem(meridiem){
-  if(hours < 0 || hours > 12) 
+Time::Time(int hours, int minutes, int seconds, string meridiem) : hours(hours), minutes(minutes), seconds(seconds), meridiem(meridiem)
+{
+  if (hours < 0 || hours > 12)
     throw invalid_argument("La hora ingresada es inválida: debe estar entre 0 y 12");
-  if(minutes < 0 || minutes > 59) 
+  if (minutes < 0 || minutes > 59)
     throw invalid_argument("Los minutos ingresados son inválidos: deben estar entre 0 y 59");
-  if(seconds < 0 || seconds > 59) 
+  if (seconds < 0 || seconds > 59)
     throw invalid_argument("Los segundos ingresados son inválidos: deben estar entre 0 y 59");
-  if(meridiem != "a.m." && meridiem != "p.m.") 
+  if (meridiem != "a.m." && meridiem != "p.m.")
     throw invalid_argument("Meridiano inválido: debe ser \"a.m.\" o \"p.m.\"");
-  
 }
-  
-void Time::sethour(int newhour) {
-  if(newhour < 0 || newhour > 12)
+
+void Time::sethour(int newhour)
+{
+  if (newhour < 0 || newhour > 12)
     throw invalid_argument("La hora ingresada es inválida: debe estar entre 0 y 12");
   hours = newhour;
 }
 
-void Time::setminutes(int newminutes) {
-  if(newminutes < 0 || newminutes > 59)
+void Time::setminutes(int newminutes)
+{
+  if (newminutes < 0 || newminutes > 59)
     throw invalid_argument("Los minutos ingresados son inválidos: deben estar entre 0 y 59");
   minutes = newminutes;
 }
 
-void Time::setseconds(int newseconds) {
-  if(newseconds < 0 || newseconds > 59)
+void Time::setseconds(int newseconds)
+{
+  if (newseconds < 0 || newseconds > 59)
     throw invalid_argument("Los segundos ingresados son inválidos: deben estar entre 0 y 59");
   seconds = newseconds;
 }
 
-void Time::setmeridian(string newmeridian) {
-  if(newmeridian != "a.m." && newmeridian != "p.m.")
-  throw invalid_argument("Meridiano inválido: debe ser \"a.m.\" o \"p.m.\"");
+void Time::setmeridian(string newmeridian)
+{
+  if (newmeridian != "a.m." && newmeridian != "p.m.")
+    throw invalid_argument("Meridiano inválido: debe ser \"a.m.\" o \"p.m.\"");
   meridiem = newmeridian;
 }
 
-
-void Time::setTime(int newhour, int newseconds, int newminutes, string newmeridian){
-  sethour(newhour);       
-  setminutes(newminutes); 
-  setseconds(newseconds); 
-  setmeridian(newmeridian); 
+void Time::setTime(int newhour, int newseconds, int newminutes, string newmeridian)
+{
+  sethour(newhour);
+  setminutes(newminutes);
+  setseconds(newseconds);
+  setmeridian(newmeridian);
 }
 
-int Time::gethour() const{
+int Time::gethour() const
+{
   return hours;
 }
 
-int Time::getminutes() const{
+int Time::getminutes() const
+{
   return minutes;
 }
 
-int Time::getseconds() const{
+int Time::getseconds() const
+{
   return seconds;
 }
 
-string Time::getmeridiem() const{
+string Time::getmeridiem() const
+{
   return meridiem;
 }
 
-string Time::getTime() const {
+string Time::getTime() const
+{
   ostringstream out;
   out << setw(2) << setfill('0') << hours << "h, "
       << setw(2) << setfill('0') << minutes << "m, "
@@ -73,16 +82,18 @@ string Time::getTime() const {
   return out.str();
 }
 
-
-// CORREGIR ESTE
-void Time::showTime() const{
+void Time::showTime() const
+{
   cout << setfill('0');
   int hour24;
-    
-  if (meridiem == "a.m.") {
-      hour24 = (hours == 12) ? 0 : hours; 
-  } else {
-      hour24 = (hours == 12) ? 12 : hours + 12;
+
+  if (meridiem == "a.m.")
+  {
+    hour24 = (hours == 12) ? 0 : hours;
   }
-  cout << setw(2) << hour24 << "h:" << setw(2) << minutes << "m:" << setw(2) << seconds << "s"<<endl;
+  else
+  {
+    hour24 = (hours == 12) ? 12 : hours + 12;
+  }
+  cout << setw(2) << hour24 << "h:" << setw(2) << minutes << "m:" << setw(2) << seconds << "s" << endl;
 }
--- ej1/Time.h
+++ ej1/Time.h
@@ -4,29 +4,30 @@
 #include <string>
 using std::string;
 
-class Time{
-  private:
-    int hours;
-    int minutes;
-    int seconds;
-    string meridiem;
-  public:
+class Time
+{
+private:
+  int hours;
+  int minutes;
+  int seconds;
+  string meridiem;
 
-    Time(int hours = 0, int minutes = 0, int seconds = 0, string meridiem = "a.m.");
+public:
+  Time(int hours = 0, int minutes = 0, int seconds = 0, string meridiem = "a.m.");
 
-    void sethour(int newhour);
-    void setminutes(int newminutes);
-    void setseconds(int newseconds);
-    void setmeridian(string newmeridian);
-    void setTime(int newhour, int newminutes, int newseconds, string newmeridian);
+  void sethour(int newhour);
+  void setminutes(int newminutes);
+  void setseconds(int newseconds);
+  void setmeridian(string newmeridian);
+  void setTime(int newhour, int newminutes, int newseconds, string newmeridian);
 
-    int gethour() const;
-    int getminutes() const;
-    int getseconds() const;
-    string getmeridiem() const;
-    string getTime() const;
+  int gethour() const;
+  int getminutes() const;
+  int getseconds() const;
+  string getmeridiem() const;
+  string getTime() const;
 
-    void showTime() const;
+  void showTime() const;
 };
 
 #endif // TIME_H
